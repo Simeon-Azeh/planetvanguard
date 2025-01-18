caa0c9fe878501d0acde3766b15801ece14343bf
Simeon-Azeh <s.kongnyuy@alustudent.com>
2025-01-18 14:57:20 +0100

    Update Firebase configuration and add analytics support

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFYV1Oau7G89ilBS3kSvDjrrA4JSdsa50",
-  authDomain: "pvanguard-9dae5.firebaseapp.com",
-  projectId: "pvanguard-9dae5",
-  storageBucket: "pvanguard-9dae5.firebasestorage.app",
-  messagingSenderId: "110221483383",
-  appId: "1:110221483383:web:216636c5a57d87eb1bd572",
-  measurementId: "G-VBDDBD2XYD"
+  apiKey: "AIzaSyCDh-P90vaxTi4PDNvXh6TbDnOY-0DDPAY",
+  authDomain: "planetvanguard-8b641.firebaseapp.com",
+  projectId: "planetvanguard-8b641",
+  storageBucket: "planetvanguard-8b641.firebasestorage.app",
+  messagingSenderId: "211434275461",
+  appId: "1:211434275461:web:7851d1664270f65ea15fec",
+  measurementId: "G-N237TK7CPK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let analytics;
+if (typeof window !== "undefined" && isSupported()) {
+  analytics = getAnalytics(app);
+}
 const db = getFirestore(app);
 
 export { app, db };
