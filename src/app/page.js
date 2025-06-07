501302580be08eb52bb8016f9f23db35f1c01a54
Simeon-Azeh <s.kongnyuy@alustudent.com>
2025-06-07 05:36:08 +0100

    Refactor Home component to include Header, Hero, AboutPreview, Initiatives, SuccessStories, CTA, and Footer with scroll animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import ComingSoon from './components/comingsoon'
+"use client"
+import AnimateOnScroll from './components/AnimateOnScroll';
+import AboutPreview from './components/AboutUsPreview';
+import CTA from './components/CTA';
+import Footer from './components/footer';
+import Header from './components/header';
+import Hero from './components/hero';
+import Initiatives from './components/Initiatives';
+import SuccessStories from './components/SuccessStories';
+
 
 function Home() {
   return (
     <div>
-      <ComingSoon />
+    <Header />
+         <Hero />
+         <AnimateOnScroll>
+           <AboutPreview />
+         </AnimateOnScroll>
+         <AnimateOnScroll>
+           <Initiatives />
+         </AnimateOnScroll>
+         <AnimateOnScroll>
+           <SuccessStories />
+         </AnimateOnScroll>
+         <AnimateOnScroll>
+           <CTA />
+         </AnimateOnScroll>
+         <AnimateOnScroll>
+           <Footer />
+         </AnimateOnScroll>
     </div>
   )
 }
